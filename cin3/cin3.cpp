dfd098945d0077116f9b4fda9401c60b49c1c5ac
HP <HP@DESKTOP-NMBILJD>
2024-11-02 19:13:17 +0100

    Add project files.

---
+++ cin3/cin3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int age ;
+	int myageaftare5years ;
+
+
+	cout << " please enter the age " << endl;
+	cin >> age;
+
+	cout << " please enter the  a gea ftare 5 years " << endl;
+	cin >> myageaftare5years;
+
+
+
+	cout << " my age now " << age << endl;
+	cout << " my age aftare 5 years I wiil be " << myageaftare5years << endl;
+
+
+	return 0;
+	}
